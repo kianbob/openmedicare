3b9ea9714d6e4a54cb46ed786013c0165d1dfe7f
Kian O'Connor <kianoconnor@MacBook-Pro.local>
2026-02-21 03:58:07 -0800

    Add BreadcrumbList JSON-LD schema for investigations

---
+++ src/app/investigations/layout.tsx
@@ -0,0 +1,34 @@
+export default function InvestigationsLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const breadcrumbJsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'BreadcrumbList',
+    itemListElement: [
+      {
+        '@type': 'ListItem',
+        position: 1,
+        name: 'Home',
+        item: 'https://www.openmedicare.org',
+      },
+      {
+        '@type': 'ListItem',
+        position: 2,
+        name: 'Investigations',
+        item: 'https://www.openmedicare.org/investigations',
+      },
+    ],
+  }
+
+  return (
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(breadcrumbJsonLd) }}
+      />
+      {children}
+    </>
+  )
+}
