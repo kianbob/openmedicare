db12e2b40b13a1be107d664072cac2977cc533a5
Kian O'Connor <kianventures@gmail.com>
2026-08-24 15:09:43 -0700

    Remove Google AdSense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -121,8 +121,7 @@ export default function RootLayout({
             </Script>
           </>
         )}
-        <meta name="google-adsense-account" content="ca-pub-9872374508496229" />
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9872374508496229" crossOrigin="anonymous"></script>      </head>
+      </head>
       <body className={`${inter.variable} ${playfair.variable} font-sans bg-gray-50 text-gray-900 antialiased`}>
         <Navigation />
         <main className="min-h-screen">
